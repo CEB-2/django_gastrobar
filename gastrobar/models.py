62d28adf9fdad829a2dd95d792a949c0d4434d5f
omuculaptele <florin.moldoveanu.ceb@immune.institute>
2022-03-28 13:05:40 +0200

    models

--- gastrobar/models.py
+++ gastrobar/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=254),
+    mail = models.EmailField(max_length=254),
+    phone = models.CharField(max_length=15),
+    date = models.DateField(max_length=20),  # auto_now_add = True, blank=True,
+    time = models.TimeField(max_length=20),  # auto_now_add = True, blank=True,
+    count_p = models.IntegerField(max_length=12)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=254),
+    description = models.TextField(),
+    price = models.CharField(max_length=15),
+    photo = models.IntegerField(max_length=12)
--- gastroblog/models.py
+++ gastroblog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    content = models.CharField(max_length=254)
+
+
+class Comments(models.Model):
+    id_article = models.IntegerField(max_length=254),
+    user = models.CharField(max_length=254),
+    comment = models.TextField(),
+    # foreign key a lo mejor?!
