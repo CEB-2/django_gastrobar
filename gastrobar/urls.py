3a54dcb52c2e1549c9960172c878f19b34efa710
jaimevelascob <jaimevelasco3@hotmail.com>
2022-03-28 12:45:35 +0200

    ad

--- gastrobar/urls.py
+++ gastrobar/urls.py
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from gastrobar import urls
-from gastroblog import urls
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('gastrobar/', include('gastrobar.urls')),
-    path('gastroblog/', include('gastroblog.urls')),
+    path('', views.home, name='home')
 ]
---
+++ gastroblog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GastroblogConfig(AppConfig):
+    name = 'gastroblog'
