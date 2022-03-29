f83c147ab86eda06c471c4cf4f62c3ba74d46247
Forby02 <adrian.barbeito@immune.institute>
2022-03-29 13:06:16 +0200

    Carga BBDD y resultos problemas

--- gastrobar/models.py
+++ gastrobar/models.py
@@ -1,20 +1,23 @@
+from __future__ import unicode_literals
+
 from django.db import models
+
 
 # Create your models here.
 
 
 class Reservation(models.Model):
-    name = models.CharField(max_length=20),
-    mail = models.EmailField(max_length=50),
-    phone = models.CharField(max_length=15),
-    date = models.DateField(max_length=25),
-    time = models.TimeField(max_length=5),
+    name = models.CharField(max_length=20, null = False, default = None)
+    mail = models.EmailField(max_length=50, null = False, default = None)
+    phone = models.CharField(max_length=15, null = False, default = None)
+    date = models.DateField(max_length=25, null = False, default = None)
+    time = models.TimeField(max_length=5, null = False, default = None)
     count_p = models.IntegerField()
 
 
 class Dish(models.Model):
-    name = models.CharField(max_length=100),
-    type = models.CharField(max_length=25)
-    description = models.TextField(),
-    price = models.CharField(max_length=15),
-    photo = models.IntegerField(max_length=12)
+    name = models.CharField(max_length=100, null = False, default = None)
+    type = models.CharField(max_length=25, null = False, default = None)
+    description = models.TextField(null = False, default = None)
+    price = models.CharField(max_length=15, null = False, default = None)
+    photo = models.CharField(max_length=12, null = False, default = None)
--- gastroblog/models.py
+++ gastroblog/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class Article(models.Model):
-    content = models.TextField()
+    content = models.TextField(null = False, default = None)
 
 
 class Comments(models.Model):
-    id_article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    user = models.CharField(max_length = 20),
-    comment = models.TextField()
+    id_article = models.ForeignKey(Article, on_delete=models.CASCADE, null = False, default = None)
+    user = models.CharField(max_length = 20, null = False, default = None)
+    comment = models.TextField(null = False, default = None)
