2cceb6daf1410f524f7495d8f2160c2ead440d1d
Dani <daniel.gonzalez@immune.institute>
2022-03-29 13:19:12 +0200

    navegacion

--- gastrobar/urls.py
+++ gastrobar/urls.py
@@ -4,11 +4,7 @@ from gastrobar import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
-    path('bar', views.bar, name='menu'),
-=======
-    path('bar', views.bar, name='bar'),
->>>>>>> 054dac918c4b05e9403323daf8af7dc6fb99e11e
+    path('bar', views.bar, name='menubar'),
     path('dish', views.dish, name='dish'),
     path('reservation', views.reservation, name='reservation'),
     
