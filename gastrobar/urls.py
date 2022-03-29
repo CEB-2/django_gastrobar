054dac918c4b05e9403323daf8af7dc6fb99e11e
Natalia <lisnatalia2002.lnmc@gmail.com>
2022-03-29 13:15:53 +0200

    merges corregidos

--- gastrobar/urls.py
+++ gastrobar/urls.py
@@ -4,16 +4,8 @@ from gastrobar import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
-    #path('', TemplateView().as_view(template_name='main.html')),
-    #path('', views.bar, name='bar'),
-    path('dish', views.dish, name='dish'),
-    #path('<int:pk>/', views.reservation, name='book'),
-    #path('', views.reservation, name='reservation'),  
-=======
     path('bar', views.bar, name='bar'),
     path('dish', views.dish, name='dish'),
     path('reservation', views.reservation, name='reservation'),
     
->>>>>>> 8191abaa8b346af017af397adc671765612113e2
 ]
