67769b5918051779e5d211735ee29d56b447f952
omuculaptele <florin.moldoveanu.ceb@immune.institute>
2022-03-30 12:57:17 +0200

    urls

--- gastrobar/urls.py
+++ gastrobar/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from gastrobar import views
 
 urlpatterns = [
-    path('', views.main, name='carta'),
+    path('', views.carta, name='carta'),
     path('menu', views.menu, name='menu_dia'),
     path('carta', views.carta, name='carta'),
     path('dish', views.dish, name='dish'),
