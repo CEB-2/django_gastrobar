ef9d7bc954de9bc57f618b65c3a2b3116f4126c8
daviz2402 <david.martinez.ceb@immune.institute>
2022-03-30 21:05:55 +0200

    view de blog para los comentarios

--- gastroblog/views.py
+++ gastroblog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from gastroblog.forms import CommentsForm
+from gastroblog.models import Comments
 
 def blog(request):
+    form = CommentsForm()
 
-    return render(request, "blog.html")
+    if request.method == 'POST':
+        form = CommentsForm(request.POST)
+        if form.is_valid():
+            new_comment = Comments(
+                id_article = form.cleaned_data['id_article'],
+                user = form.cleaned_data['user'],
+                comment = form.cleaned_data['comment'],
+            )
+            new_comment.save()
+    context = {
+       
+        'form' : form
+    }
+    return render(request, 'blog.html', context)
 # Create your views here.
 
