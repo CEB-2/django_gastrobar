0cace4fe1139bee49980f0e544308401767760e8
jaimevelascob <jaimevelasco3@hotmail.com>
2022-03-31 12:42:07 +0200

    seed + connect menu

--- gastrobar/views.py
+++ gastrobar/views.py
@@ -15,10 +15,8 @@ def menu(request):
 	menu = menu.objects.all()
 	context = {
 		'menu' : menu,
-        
 	}
-
-	return render(request, 'menu.html', context)
+    return render(request, 'menu.html', context)
 
 def carta(request):
 	cartaFirst = Dish.objects.filter(type="first")
@@ -29,17 +27,25 @@ def carta(request):
 		'cartaFirst' : cartaFirst,
 		'cartaSecond' : cartaSecond,
 		'cartaThird' : cartaThird,
-        
 	}
 	return render(request, 'carta.html', context)
 
 def dish(request):
 	dish = Dish.objects.all()
-	
+    # seed values
+    first = calc_menu(1,3)
+    second = calc_menu(4,6)
+    dessert = calc_menu(7,9)
+    # dish values
+    dish_first = Dish.objects.get(pk=first)
+    dish_second =Dish.objects.get(pk=second) 
+    dish_dessert =Dish.objects.get(pk=dessert) 
 	context = {
-		'dish' : dish,
-        
-	}
+		'dish_first' :  dish_first, 
+	    'dish_second':  dish_second, 
+        'dish_dessert': dish_dessert,
+    }
+    # pasar primary key de los platos
 	return render(request, 'dish.html', context)
 
 
@@ -67,4 +73,6 @@ def reservation(request):
 
 	return render(request, 'reservation.html', context)
 
-	
+def calc_menu(val1, val2):
+    random.seed(datetime.today().strftime("gY-&m-%d"))
+    return random. randint(val1, val2)
