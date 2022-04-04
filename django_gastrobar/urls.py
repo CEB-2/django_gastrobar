e50822587e8982c7dcb24c37fbdf644c84d31a24
Dani <daniel.gonzalez@immune.institute>
2022-04-04 13:23:30 +0200

    navegacion gastroempleo desde home

--- django_gastrobar/urls.py
+++ django_gastrobar/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('legalAdvice/',views.legal, name='legal'),
     path('privacy/',views.privacy, name='privacy'),
     path('contact/',views.contact, name='contact'),
+    path('gastroempleo/',include('gastroempleo.urls')),
 ]
