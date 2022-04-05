7895d82976ed1a81253704ccf96310ba3cc32da4
Dani <daniel.gonzalez@immune.institute>
2022-04-05 10:36:46 +0200

    navegacion aviso legal y base general

--- django_gastrobar/views.py
+++ django_gastrobar/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+def legal(request):
+	return render(request, 'legal.html')
+
+def privacy(request):
+	return render(request, 'privacy.html') 
